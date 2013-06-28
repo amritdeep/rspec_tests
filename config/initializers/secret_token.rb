3436b1c580aff53a3d74a7485e080f973eb9322e
amrit <amrit@lenovo.(none)>
2013-06-28 09:56:35 +0545

    Initial project and bundle the required gem package

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspectTest::Application.config.secret_token = '86e3d2f42a8f8f84da24221c4b0623950bbfc74701149a6cfd4b5344a3f69d1ba9c89ba41dac1208429f68df115b9c13b8a9958393fe0c110693183f7dd7f430'
