ee20bbc017e1b09533fb9a61d63c39f52c78dfcc
amrit <amrit@lenovo.(none)>
2013-06-28 10:06:53 +0545

    add static page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RspectTest::Application.routes.draw do
+  get "page/home"
+
+  get "page/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +53,7 @@ RspectTest::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'page#home'
 
   # See how all your routes lay out with "rake routes"
 
