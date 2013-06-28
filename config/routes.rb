40b39775c832d6640ee159fc14e7206c996339c0
amrit <amrit@lenovo.(none)>
2013-06-28 15:13:02 +0545

    add contact page and write rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 RspectTest::Application.routes.draw do
-  get "page/home"
 
-  get "page/help"
+  match "/home" => "page#home", as: :home, via: :get
+  match "/help" => "page#help", as: :help, via: :get
+  match "/about" =>"page#about", as: :about, via: :get  
+  match "/contact" => "page#contact", as: :contact, via: :get
 
-  match "page/about" =>"page#about", as: :about, via: :get  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -55,7 +56,7 @@ RspectTest::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'page#home'
+  root to: 'page#home'
 
   # See how all your routes lay out with "rake routes"
 
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -4,31 +4,39 @@ describe "Pages" do
   describe "GET /pages" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get page_home_path
+      get root_url
       response.status.should be(200)
     end
 
-    let(:base_title){ "RspectTest" }
+    #let(:base_title){ "RspectTest" }
+
+    subject { page }
 
     describe "Home page" do 
-    	it "should have some h1 'Home'" do
-    		visit '/page/home'
-    		page.should have_selector('h1', text: 'Home')
-   		end
+    	before { visit home_url }
+    	it { should have_selector('h1', text: 'Home') }
+
     end
 
     describe "Help page" do
-    	it "should have some h1 'Help''" do
-    		visit '/page/help'
-    		page.should have_selector('h1', text: 'Help')
-    	end
+    	before { visit help_url}
+    	it { should have_selector('h1', text: 'Help') }
     end 
 
     describe "About page" do
-    	it "should have some h1 'About Us'" do
-    		visit '/page/about'
-    		page.should have_selector('h1', text: "About Us")
-    	end
+    	before { visit about_url }
+    	it { should have_selector('h1', text: 'About Us') }
+    end
+
+    describe "Contact Page" do
+    	before { visit contact_url }
+    	it { should have_selector('h1', text: 'Contact') }
+
+    	# it "should have title 'Contact'" do
+    	# 	visit contact_url
+    	# 	page.should have_selector('title', text: "'#{:base_title}'| Contact")
+    	# end
+
     end
 
   end
